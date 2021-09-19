ff3379b7c9cdcab1911193197df65baebde1d8d7
sntanju <sntanju07@gmail.com>
2021-09-19 16:59:57 +0600

    last-commit

--- js/mealDB.js
+++ js/mealDB.js
@@ -1,3 +1,14 @@
+const  searchBtn = document.getElementById("button-search");
+const searchInput = document.getElementById("search-field");
+
+searchInput.addEventListener("keypress", function(event) {
+    //event.preventDefault();
+    if (event.keyCode == enter) {
+        searchBtn.click();
+    }
+
+});
+
 const searchFood = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
